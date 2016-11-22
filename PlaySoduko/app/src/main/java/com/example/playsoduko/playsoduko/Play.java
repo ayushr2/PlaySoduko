161779f211150d137371dd188550c6d393da5c2f
ayushr2 <ayushr2@illinois.edu>
2016-11-21 23:44:38 -0500

    play option interface

--- PlaySoduko/app/src/main/java/com/example/playsoduko/playsoduko/Play.java
+++ PlaySoduko/app/src/main/java/com/example/playsoduko/playsoduko/Play.java
@@ -11,9 +11,63 @@ import android.view.View;
 import android.widget.Button;
 
 public class Play extends AppCompatActivity {
+
+    Button currentlySelected = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_layout);
+        final Button oneDif = (Button) findViewById(R.id.one);
+        final Button twoDif = (Button) findViewById(R.id.two);
+        final Button threeDif = (Button) findViewById(R.id.three);
+        final Button fourDif = (Button) findViewById(R.id.four);
+        final Button fiveDif = (Button) findViewById(R.id.five);
+
+        oneDif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oneDif.setPressed(true);
+                if(currentlySelected != null)
+                    currentlySelected.setPressed(false);
+                currentlySelected = oneDif;
+            }
+        });
+        twoDif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                twoDif.setPressed(true);
+                if(currentlySelected != null)
+                    currentlySelected.setPressed(false);
+                currentlySelected = twoDif;
+            }
+        });
+        threeDif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                threeDif.setPressed(true);
+                if(currentlySelected != null)
+                    currentlySelected.setPressed(false);
+                currentlySelected = threeDif;
+            }
+        });
+        fourDif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fourDif.setPressed(true);
+                if(currentlySelected != null)
+                    currentlySelected.setPressed(false);
+                currentlySelected = fourDif;
+            }
+        });
+        fiveDif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fiveDif.setPressed(true);
+                if(currentlySelected != null)
+                    currentlySelected.setPressed(false);
+                currentlySelected = fiveDif;
+            }
+        });
+
     }
 }
